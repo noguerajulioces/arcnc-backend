514671dd0c01ebb5d9ecf63332291c5086f2ffff
Julio Noguera <noguerajulio24@gmail.com>
2019-10-01 22:06:08 -0500

    add routes

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ const multer = require('multer');
 const uploadConfig = require('./config/Upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
-
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -12,7 +13,12 @@ const upload = multer(uploadConfig);
 routes.post('/sessions', SessionController.store);
 
 //
+routes.get('/spots', SpotController.index);
 routes.post('/spot', upload.single('thumbnail'), SpotController.store);
 
+routes.get('/dashboard', DashboardController.show);
+
+
+routes.get('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
