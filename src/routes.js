e3c6c9823de12765f2097f50200a76b00416200c
Julio Noguera <noguerajulio24@gmail.com>
2019-10-01 21:18:19 -0500

    finish spot

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/Upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 //User Session
 routes.post('/sessions', SessionController.store);
 
 //
-routes.post('/spot', SpotController.store);
+routes.post('/spot', upload.single('thumbnail'), SpotController.store);
 
 
 module.exports = routes;
