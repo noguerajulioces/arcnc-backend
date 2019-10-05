9f0a001876b06f0322d05da6d168b84dae010026
Julio Noguera <noguerajulio24@gmail.com>
2019-10-04 22:54:52 -0500

    add changes

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -5,10 +5,18 @@ const SpotSchema = new mongoose.Schema({
     company: String,
     price: Number,
     techs: [String],
-    user: [{
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-    }]
+    }
+}, {
+  toJSON: {
+    virtuals = true,
+  },
 });
+
+SpotSchema.virtual('thumbail_url').get(function(){
+  return `http://localhost:3001/files/${this.thumbnail}`
+})
 
 module.exports = mongoose.model('Spot', SpotSchema);
